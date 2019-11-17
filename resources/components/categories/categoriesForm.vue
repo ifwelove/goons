3594380931b53db622d9ec41367b685546a27694
debbyji <jasmine820921@gmail.com>
2019-11-17 19:36:59 +0800

    所有頁面加上 vue & js

---
+++ resources/components/categories/categoriesForm.vue
@@ -0,0 +1,13 @@
+<template>
+	<p>categories form</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- resources/views/accounts/edit.blade.php
+++ resources/views/accounts/edit.blade.php
@@ -1,101 +1,21 @@
 @extends('layouts.master')
 
 @section('css')
-
+    <link href="{{ URL::asset('assets/libs/sweetalert2/sweetalert2.min.css') }}" rel="stylesheet" type="text/css" />
 @endsection
 
 @section('breadcrumb')
-    <div class="row">
-        <div class="col-12">
-            <div class="page-title-box">
-                <div class="page-title-right">
-                    <ol class="breadcrumb m-0">
-                        <li class="breadcrumb-item"><a href="javascript: void(0);">{{ __('backend.setting.system') }}</a></li>
-                        <li class="breadcrumb-item"><a href="javascript: void(0);">{{ __('backend.setting.account') }}</a></li>
-                        <li class="breadcrumb-item active">{{ __('backend.edit.account') }}</li>
-                    </ol>
-                </div>
-                <h4 class="page-title">{{ __('backend.edit.account') }}</h4>
-            </div>
-        </div>
-    </div>
 @endsection
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <div class="card-box">
-                    <h4 class="header-title"></h4>
-                    <p class="sub-header">
-                    </p>
-                    {{ Form::model($account, array('route' => array('accounts.update', $account->id), 'method' => 'PUT', 'class' => 'parsley-form')) }}
-                    <div class="form-group row">
-                        <label for="name" class="col-sm-2 col-form-label">
-                            姓名
-                            <span class="text-danger">*</span>
-                        </label>
-                        <div class="col-sm-10">
-                            <input type="text" name="name" value="{{ $account->name }}" required class="form-control" id="name" placeholder="姓名">
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="email" class="col-sm-2 col-form-label">
-                            帳號
-                            <span class="text-danger">*</span>
-                        </label>
-                        <div class="col-sm-10">
-                            <input type="text" name="email" value="{{ $account->email }}" required class="form-control" id="email" placeholder="帳號" disabled>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="password" class="col-sm-2 col-form-label">
-                            密碼
-                            <span class="text-danger">*</span>
-                        </label>
-                        <div class="col-sm-10">
-                            <input type="text" name="password" class="form-control" id="password" placeholder="密碼">
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="roles" class="col-sm-2 col-form-label">
-                            權限設定
-                            <span class="text-danger">*</span>
-                        </label>
-                        <div class="col-sm-10">
-                            @foreach ($roles as $role)
-                                <div class="checkbox checkbox-primary">
-                                    {{Form::checkbox('roles[]',  $role->id, $account->roles, array('id' => $role->id)) }}
-                                    {{Form::label($role->id, ucfirst($role->slug) . '(' . ucfirst($role->name) . ')') }}
-                                </div>
-                            @endforeach
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <div class="col-sm-12 offset-sm-4">
-                            {{ Form::submit(__('backend.edit'), array('class' => 'btn btn-primary waves-effect waves-light mr-1')) }}
-                            {{ Form::reset(__('backend.cancel'), array('class' => 'btn btn-secondary waves-effect waves-light')) }}
-                        </div>
-                    </div>
-                    {{ Form::close() }}
-                </div>
-            </div>
-        </div>
-    </div>
+<div id="accounts">
+    <account-form></account-form>
+</div>
 @endsection
 
 @section('script')
-
 @endsection
 
 @section('script-bottom')
-    <!-- init js -->
-    <!-- Validation js (Parsleyjs) -->
-    <script src="{{ URL::asset('assets/libs/parsleyjs/parsleyjs.min.js') }}"></script>
-    <!-- validation init -->
-    <script src="{{ URL::asset('assets/js/pages/form-validation.init.js') }}"></script>
+<script src="{{ URL::asset('js/accounts.js') }}"></script>
 @endsection
