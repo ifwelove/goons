f443fc503255932e47b8531f229e719b2dd8e3b2
ponpon <ponpon@flydove.net>
2019-11-19 00:29:53 +0800

    編輯帳號新增帳號api

--- resources/views/accounts/create.blade.php
+++ resources/views/accounts/create.blade.php
@@ -36,7 +36,7 @@
                             <span class="text-danger">*</span>
                         </label>
                         <div class="col-sm-10">
-                            <input type="text" name="name" required class="form-control" id="name" placeholder="姓名" disabled>
+                            <input type="text" name="name" required class="form-control" id="name" placeholder="姓名">
                         </div>
                     </div>
 
@@ -46,7 +46,7 @@
                             <span class="text-danger">*</span>
                         </label>
                         <div class="col-sm-10">
-                            <input type="text" name="email" required class="form-control" id="email" placeholder="帳號" disabled>
+                            <input type="text" name="email" required class="form-control" id="email" placeholder="帳號">
                         </div>
                     </div>
 
@@ -56,7 +56,7 @@
                             <span class="text-danger">*</span>
                         </label>
                         <div class="col-sm-10">
-                            <input type="text" name="password" required class="form-control" id="password" placeholder="密碼" disabled>
+                            <input type="text" name="password" required class="form-control" id="password" placeholder="密碼">
                         </div>
                     </div>
 
