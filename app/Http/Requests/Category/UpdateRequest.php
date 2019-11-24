3aec3b00a486f3bddf7412efdbbfb6fab0e1aec7
ponpon <ponpon@flydove.net>
2019-11-24 18:55:25 +0800

    category api

---
+++ app/Http/Requests/Category/UpdateRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use App\Http\Requests\BasicRequest;
+use App\Models\Category;
+
+class UpdateRequest extends BasicRequest
+{
+    public function __construct()
+    {
+    }
+
+    public function authorize()
+    {
+        return Category::find($this->route('category'));
+    }
+
+    public function rules()
+    {
+        return [
+//            'roles' => 'required|exists:roles,id',
+        ];
+    }
+}
