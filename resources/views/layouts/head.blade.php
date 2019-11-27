58771216a11e94ceb88a0fd924dd9a3779c14ac7
debbyji <jasmine820921@gmail.com>
2019-11-27 23:07:14 +0800

    共用 css & script 移至 head & bottom

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -3,6 +3,7 @@
 @yield('css')
 
 <!-- App css -->
+<link href="{{ URL::asset('assets/libs/sweetalert2/sweetalert2.min.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ URL::asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css"/>
 <!-- <link href="{{ URL::asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/> -->
 <!-- <link href="{{ URL::asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css"/> -->
