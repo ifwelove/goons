8037cb97855e17c740f18ed951a8a1610bd03cc1
ponpon <ponpon@flydove.net>
2019-11-27 21:53:14 +0800

    append to news model

--- app/Models/News.php
+++ app/Models/News.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -16,5 +17,33 @@ class News extends Model
         'auto_push', 'title', 'description', 'type', 'start_date', 'end_date'
     ];
     protected $dates = ['deleted_at'];
+    protected $appends = [
+        'status', 'push_status'
+    ];
 
+    public function getStatusAttribute()
+    {
+        if (Carbon::now() > $this->end_date) {
+            return '已下架';
+        }
+        if (Carbon::now() >= $this->start_date && Carbon::now() <= $this->end_date) {
+            return '已上架';
+        }
+        if (Carbon::now() <= $this->start_date) {
+            return '預約中';
+        }
+    }
+
+    public function getPushStatusAttribute()
+    {
+        if ($this->type) {
+            return '已推播';
+        }
+        if($this->auto_push) {
+            return '是';
+        }
+        if(!$this->auto_push) {
+            return '否';
+        }
+    }
 }
