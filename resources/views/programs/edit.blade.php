200af3c1bab57811fc932359176b06ee1ef5601a
debbyji <jasmine820921@gmail.com>
2019-11-30 23:35:14 +0800

    調整 blade

--- resources/views/programs/edit.blade.php
+++ resources/views/programs/edit.blade.php
@@ -2,8 +2,7 @@
 
 @section('css')
 <link href="{{ URL::asset('assets/libs/bootstrap-select/bootstrap-select.min.css') }}" rel="stylesheet" type="text/css">
-<link href="{{ URL::asset('assets/libs/bootstrap-datepicker/bootstrap-datepicker.min.css') }}" rel="stylesheet" type="text/css">
-<link href="{{ URL::asset('assets/libs/sweetalert2/sweetalert2.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('assets/libs/bootstrap-datetimepicker/bootstrap-datetimepicker.css') }}" rel="stylesheet" type="text/css">
 @endsection
 
 @section('breadcrumb')
@@ -11,7 +10,7 @@
 
 @section('content')
 <div id="programs">
-    <program-form :is-edit="true"></program-form>
+    <program-form :is-edit="true" :program-id="{{ $id }}"></program-form>
 </div>
 @endsection
 
@@ -20,6 +19,6 @@
 
 @section('script-bottom')
 <script src="{{ URL::asset('assets/libs/bootstrap-select/bootstrap-select.min.js') }}"></script>
-<script src="{{ URL::asset('assets/libs/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
+<script src="{{ URL::asset('assets/libs/bootstrap-datetimepicker/bootstrap-datetimepicker.js') }}"></script>
 <script src="{{ URL::asset('js/programs.js') }}"></script>
 @endsection
