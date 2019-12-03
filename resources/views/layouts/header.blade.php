adc693f8fda70db5e5ba649f7b0fd6546a133448
ponpon <ponpon@flydove.net>
2019-12-03 22:19:07 +0800

    左側權限打開, sm logo移除

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -36,8 +36,8 @@
                    {{--                   <span class="logo-lg-text-light">{{ env('APP_NAME') }}</span>--}}
                </span>
                <span class="logo-sm">
-{{--                   <span class="logo-sm-text-dark">X</span>--}}
-                                      <img src="{{ URL::asset('assets/images/logo-sm.png') }}" alt="" height="24">
+                   <span class="logo-sm-text-dark">X</span>
+{{--                                      <img src="{{ URL::asset('assets/images/logo-sm.png') }}" alt="" height="24">--}}
                </span>
            </a>
        </div>
