639428437fb938ef4c54c77773d5b31add488179
debbyji <jasmine820921@gmail.com>
2019-12-03 22:57:19 +0800

    最新消息 done

--- resources/views/news/create.blade.php
+++ resources/views/news/create.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.master')
 
 @section('css')
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link href="{{ URL::asset('assets/libs/bootstrap-datetimepicker/bootstrap-datetimepicker.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ URL::asset('assets/libs/summernote/summernote.min.css') }}" rel="stylesheet" type="text/css">
 @endsection
 
 @section('breadcrumb')
@@ -16,5 +19,10 @@
 @endsection
 
 @section('script-bottom')
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="{{ URL::asset('assets/libs/bootstrap-datetimepicker/bootstrap-datetimepicker.js') }}"></script>
+<script src="{{ URL::asset('assets/libs/summernote/summernote.min.js') }}"></script>
 <script src="{{ URL::asset('js/news.js') }}"></script>
 @endsection
--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.master')
 
 @section('css')
+<link href="{{ URL::asset('assets/libs/bootstrap-select/bootstrap-select.min.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ URL::asset('assets/libs/bootstrap-daterangepicker/bootstrap-daterangepicker.min.css') }}" rel="stylesheet" type="text/css">
 @endsection
 
 @section('breadcrumb')
@@ -16,5 +18,8 @@
 @endsection
 
 @section('script-bottom')
+<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+<script src="{{ URL::asset('assets/libs/bootstrap-daterangepicker/bootstrap-daterangepicker.min.js') }}"></script>
+<script src="{{ URL::asset('assets/libs/bootstrap-select/bootstrap-select.min.js') }}"></script>
 <script src="{{ URL::asset('js/news.js') }}"></script>
 @endsection
