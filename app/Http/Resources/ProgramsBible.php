18f37ff1630ca4d4e6544c9745f5c9822ebc4391
ponpon <ponpon@flydove.net>
2019-12-04 10:54:37 +0800

    add /

--- app/Http/Resources/ProgramsBible.php
+++ app/Http/Resources/ProgramsBible.php
@@ -25,7 +25,7 @@ class ProgramsBible extends JsonResource
             'date'      => Carbon::create($this->start_date)->timestamp,
             'length'    => $this->duration,
             'playerURL' => str_replace(' ', '%20', str_replace('s//o', 's/o', $this->url)),
-            'shareURL'  => config('app.url') . $request->programType . '/' . $this->categories . '/' . $this->id,
+            'shareURL'  => config('app.url') . '/' . $request->programType . '/' . $this->categories . '/' . $this->id,
             'shareText' => $this->title . '一起來收聽吧',
         ];
     }
